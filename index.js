a630875314946da3adf43980dfb314b8bf74831c
MarkBrianKariuki <markbrian.karanja@student.moringaschoool.com>
2022-06-07 11:42:50 +0300

    Defined function Monday Work

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function saturdayFun(thing = "roller-skate"){
     return `This Saturday, I want to ${thing}!`
 }
+function mondayWork(todo = "go to the office"){
+    return `This Monday, I will ${todo}.`
+}
